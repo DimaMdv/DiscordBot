05c284cec4436326ec8cdcdaf1fea5674200c31e
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-02-06 18:19:42 -0600

    Add echo command

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='$')#prefix to all commands
+bot = commands.Bot(command_prefix='!')#prefix to all commands
 
 #comand exsample
 # @bot.command()
 # async def test(ctx):
-#     pass
+#     await dothings
+
+async def echo(ctx, arg):
+    await ctx.send(arg)
+
+bot.run('token') #important!
