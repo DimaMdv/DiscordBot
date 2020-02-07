4e85b6391f79c2d65a8d37a0e97c3654d178eb60
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-02-06 18:27:14 -0600

    Add elp command

--- bot.py
+++ bot.py
@@ -4,12 +4,23 @@ description = 'This is a simple bot for testing features'
 
 bot = commands.Bot(command_prefix='!', description=description)#prefix to all commands
 
+commands = [
+    "!help - display all commands",
+    "!echo 'arg' - return arg"
+]
+
 #comand exsample
 # @bot.command()
 # async def test(ctx):
 #     await dothings
 
+@bot.command()
+async def help(ctx):
+    for command in commands:
+        await ctx.send(command)
+
+@bot.command()
 async def echo(ctx, arg):
-    await ctx.send(arg)
+    ctx.send(arg)
 
 bot.run('token') #important!
