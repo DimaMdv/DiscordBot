5f8de5641375bbdc457ccd8ef93cd8a94c2f7816
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-02-08 17:58:16 -0600

    minor changes

--- bot.py
+++ bot.py
@@ -2,14 +2,10 @@ from discord.ext import commands
 from random import randint as rint
 
 description = 'This is a simple bot for testing features' #description of the bot
-commands_pref = '!' #commands prefix
+commands_prefix = '!' #commands prefix
 
-bot = commands.Bot(command_prefix=commands_pref, description=description)#prefix to all commands
+bot = commands.Bot(command_prefix=commands_prefix, description=description)#prefix to all commands
 
-#comand exsample
-# @bot.command()
-# async def test(ctx):
-#     await dothings
 
 @bot.event #calls when bot launched
 async def on_ready(ctx):
@@ -26,19 +22,22 @@ async def on_member_join(ctx, member):
         to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
         await guild.system_channel.send(to_send)
 
-@bot.command(description="return random number betwen 1 and an argument")
+
+@bot.command(description='return random number betwen 1 and an argument')
+#description is seeing only on !help command
 async def dice(ctx, arg:int):
     try:
         await ctx.send(rint(1, arg))
     except Exception:
         await ctx.send('Oops, seems its not a number!')
 
-@bot.command()
+@bot.command(description='return "pong"')
 async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command(description='reply with the same message')
 async def echo(ctx, arg):
     ctx.send(arg)
+
 
 bot.run('token') #important!
