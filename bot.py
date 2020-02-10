0807b572009364b6553cb16bce824401b598c19b
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-02-09 21:45:31 -0600

    Bot now can give a roles by reactio

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 from random import randint as rint
 
 description = 'This is a simple bot for testing features' #description of the bot
@@ -14,6 +15,14 @@ async def on_ready(ctx):
     print(bot.user.id)
     print('------')
     # await ctx.send('I\'m alive!')
+    channel = bot.get_channel('chanel id')
+    role = discord.utils.get(user.server.roles, name="rolename")
+    message = await bot.send_message(channel, "message text")
+    while True:
+        reaction = await bot.wait_for_reaction(emoji="emoji", message=message)
+        await bot.add_roles(reaction.message.author, role)
+
+
 
 @bot.event #I dont know if its working, but I whant to belive so
 async def on_member_join(ctx, member):
@@ -22,6 +31,14 @@ async def on_member_join(ctx, member):
         to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
         await guild.system_channel.send(to_send)
 
+# @bot.event
+# async def on_ready():
+#     channel = bot.get_channel('549294402514976783')#DELETE CHANEL ID
+#     role = discord.utils.get(user.server.roles, name="tester")#ROLENAME
+#     message = await bot.send_message(channel, "React to me!")
+#     while True:
+#         reaction = await bot.wait_for_reaction(emoji=":penguin:", message=message)
+#         await bot.add_roles(reaction.message.author, role)
 
 @bot.command(description='return random number betwen 1 and an argument')
 #description is seeing only on !help command
