a15d0dcb922ca33ef1447bcd4b17c950f55d5c14
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-02-17 12:12:36 -0600

    Now it cat add a role to reaction author

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 from discord.ext import commands
-import discord
+from discord.utils import get
 from random import randint as rint
 
 description = 'This is a simple bot for testing features' #description of the bot
 commands_prefix = '!' #commands prefix
 
-bot = commands.Bot(command_prefix=commands_prefix, description=description)#prefix to all commands
+bot = commands.Bot(command_prefix=commands_prefix, description=description)
 
 
 @bot.event #calls when bot launched
@@ -16,11 +16,11 @@ async def on_ready(ctx):
     print('------')
     # await ctx.send('I\'m alive!')
     channel = bot.get_channel('chanel id')
-    role = discord.utils.get(user.server.roles, name="rolename")
-    message = await bot.send_message(channel, "message text")
+    role = get(ctx.guild.roles, name="rolename")
+    message = await ctx.send(channel, "message text")
     while True:
-        reaction = await bot.wait_for_reaction(emoji="emoji", message=message)
-        await bot.add_roles(reaction.message.author, role)
+        reaction = await ctx.wait_for_reaction(emoji="emoji", message=message)
+        await ctx.add_roles(reaction.message.author, role)
 
 
 
@@ -30,15 +30,6 @@ async def on_member_join(ctx, member):
     if guild.system_channel is not None:
         to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
         await guild.system_channel.send(to_send)
-
-# @bot.event
-# async def on_ready():
-#     channel = bot.get_channel('549294402514976783')#DELETE CHANEL ID
-#     role = discord.utils.get(user.server.roles, name="tester")#ROLENAME
-#     message = await bot.send_message(channel, "React to me!")
-#     while True:
-#         reaction = await bot.wait_for_reaction(emoji=":penguin:", message=message)
-#         await bot.add_roles(reaction.message.author, role)
 
 @bot.command(description='return random number betwen 1 and an argument')
 #description is seeing only on !help command
